da282c750ee9abf85a9391e0a3c062cf7086b0a0
chenyu <401434187@qq.com>
2021-01-27 15:34:21 +0800

    登陆

--- ourblog-parent/ourblog-user/src/main/java/com/ourblog/user/UserApplication.java
+++ ourblog-parent/ourblog-user/src/main/java/com/ourblog/user/UserApplication.java
@@ -1,8 +1,10 @@
 package com.ourblog.user;
 
+import com.ourblog.common.exception.ExceptionCatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -18,5 +20,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+    @Bean
+    public ExceptionCatch exceptionCatch(){
+        return  new ExceptionCatch();
     }
 }
