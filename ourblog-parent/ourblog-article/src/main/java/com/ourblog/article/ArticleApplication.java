80adf688e370fa8a7b589566b42619a47a65ea47
yudachi <452419829@qq.com>
2021-01-28 11:02:15 +0800

    初始化文章服务

---
+++ ourblog-parent/ourblog-article/src/main/java/com/ourblog/article/ArticleApplication.java
@@ -0,0 +1,18 @@
+package com.ourblog.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ArticleApplication
+ * @Description TODO
+ * @Author Yudachi
+ * @Date 2021/1/28 11:01
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
