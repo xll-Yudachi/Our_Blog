696a5f3421a100d830cab2de5a33a91ee7277578
yudachi <452419829@qq.com>
2021-02-02 11:28:35 +0800

    ESClient工具类

---
+++ ourblog-parent/ourblog-common/src/main/java/com/ourblog/common/utils/ElasticSearchClient.java
@@ -0,0 +1,31 @@
+package com.ourblog.common.utils;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+
+public class ElasticSearchClient {
+    public ElasticSearchClient() {}
+    public static RestHighLevelClient getClient() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("xxx", "xxx"));  //es账号密码（默认用户名为elastic）
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("xxx", 9200, "http"))
+                        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                            @Override
+                            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                                httpClientBuilder.disableAuthCaching();
+                                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                            }
+                        }));
+        return client;
+    }
+}
